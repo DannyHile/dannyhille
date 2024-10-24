84a385b0c3f99cbf8ac46e5a0635133de79ca14a
dannyhille <danny@hille.dk>
2024-10-24 14:38:48 +0200

    swing animation

--- swing/main.js
+++ swing/main.js
@@ -2,3 +2,12 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle='#050505';
 ctx.fillRect(0,0,w=1920,h=1080);
 canvas.onclick=e=>canvas.requestFullscreen();
+const {sin,cos} = Math;
+
+!(function animate(t=0){
+    t*=.001;
+    canvas.width=w;
+    ctx.fillStyle="#fff";
+    ctx.fillRect(sin(-t)*100+w/2,cos(-t)*100+h/2,50,50);
+    requestAnimationFrame(animate);    
+})();
